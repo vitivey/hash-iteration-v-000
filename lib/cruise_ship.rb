ad68af1212c4b5016573d25540bef699f888fbe8
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-11 02:08:17 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,15 +1,15 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
   # add the code snippet here!
   passengers.each do |key, value|
-    if value.first="A"
-      return value 
+    if value.first=="A"
+      return value
   end
 end
