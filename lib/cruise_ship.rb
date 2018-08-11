b58fd0a0ac123222c6c50d8fdbbcfaf744b3dc06
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-11 02:11:37 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@
 def select_winner(passengers)
   # add the code snippet here!
   passengers.each do |suite, name|
-    if suite == :suite_a & name.start_with?("A")
+    if suite == :suite_a && name.start_with?("A")
       return name
     end
   end
